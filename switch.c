7726edc96107b2b952bbe39c2f6f791ce752dbe3
Christian Gracia <christiangracia.student@careerdevs.com>
2018-12-19 22:04:52 +0000

    simple switch statement with C#

---
+++ switch.c
@@ -0,0 +1,14 @@
+int i = 2;
+
+switch (i)
+{
+    case 1:
+        Console.WriteLine("One");
+        break;
+    case 2:
+        Console.WriteLine("Two");
+        break;
+    default:
+        Console.WriteLine("Other");
+        break;
+}
