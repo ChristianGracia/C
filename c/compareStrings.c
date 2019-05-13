4049bae915fb5055793b35339377694846470a6f
Christian Gracia <christiangracia.student@careerdevs.com>
2019-05-13 18:16:14 -0400

    added short program that compares strings and returns same or different respectively

---
+++ c/compareStrings.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+bool compare_strings(string a, string b);
+
+int main(void)
+{
+    string i = get_string("i: ");
+    string j = get_string("j: ");
+
+    if (compare_strings(i, j))
+    {
+        printf("same\n");
+    }
+    else
+    {
+        printf("different\n");
+    }
+}
+
+
+bool compare_strings(string a, string b)
+{
+    if (strlen(a) != strlen(b)){
+        return false;
+    }
+    for (int i = 0, n = strlen(a); i < n; i++){
+        if (a[i] != b[i]){
+            return false;
+        }
+    }
+
+    return true;
+}
