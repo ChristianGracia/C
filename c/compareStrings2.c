2534ab2aefd408614efbe7d94cc48949b9ea5a08
Christian Gracia <christiangracia.student@careerdevs.com>
2019-05-13 18:40:30 -0400

    added error checking to prevent segmentation faults

--- c/compareStrings2.c
+++ c/compareStrings2.c
@@ -7,7 +7,16 @@ bool compare_strings(char *a, char *b);
 int main(void)
 {
     char *i = get_string("i: ");
+    if (i == NULL)
+    {
+        return 1;
+    }
+
     char *j = get_string("j: ");
+    if (j == NULL)
+    {
+        return 1;
+    }
 
     if (strcmp(i,j) == 0)
     {
@@ -17,7 +26,10 @@ int main(void)
     {
         printf("different\n");
     }
+
+    return 0;
 }
+
 
 
 //strcmp can be used not one to check if strings are equal but it returns a negative or postive number if the compared letter is unequal which can be used to alphabetize
