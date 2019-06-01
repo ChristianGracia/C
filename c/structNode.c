7e76ceb4423ab6161b228d9cc3c4a40cf79a0b6a
Christian Gracia <christiangracia.student@careerdevs.com>
2019-06-01 12:47:03 -0400

    linkined the nodes for linked list

--- c/structNode.c
+++ c/structNode.c
@@ -11,10 +11,21 @@ typedef struct NODE
 
 } NODE;
 
+void addNode(int value, NODE *homeNode)
+{
+    NODE *newNode = malloc(sizeof(NODE));
+    newNode->value = value;
+    homeNode->next = newNode;
+}
+
 int main(void)
 {
     NODE rootNode;
     rootNode.value = 5;
+    rootNode.next = NULL;
+
+    addNode(10, &rootNode);
 
     printf("%i\n", rootNode.value);
+    printf("%i\n", rootNode.next -> value);
 }
