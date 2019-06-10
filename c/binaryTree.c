e3995e8de70e7c3a15114d19888d714029c8975e
Christian Gracia <christiangracia.student@careerdevs.com>
2019-06-10 19:33:16 -0400

    added high and low value check to the binary tree

--- c/binaryTree.c
+++ c/binaryTree.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define true 1;
+#define false 0;
+
 typedef struct NODE
 {
     int value;
@@ -22,6 +25,33 @@ void addNode(int value)
         rootNode = newNode;
         return;
     }
+
+    //if value of new node is less the root node attach to low
+    //else add to high
+
+    NODE *trav = rootNode;
+
+    while(1)
+    {
+        if (newNode->value < trav->value)
+        {
+            if (trav->low == NULL)
+            {
+                trav->low = newNode;
+                return;
+            }
+            trav = trav-> low;
+            continue;
+        }
+        else
+        {
+            if (trav->high == NULL)
+            {
+                trav->high = newNode;
+                continue;
+            }
+        }
+    }
 }
 
 int main(void)
@@ -29,4 +59,13 @@ int main(void)
     addNode(10);
     addNode(8);
     addNode(15);
+    addNode(17);
+    addNode(20);
+    addNode(18);
+    addNode(5);
+    addNode(6);
+    addNode(9);
+    addNode(4);
+    addNode(13);
+    addNode(7);
 }
