72243eec8fd7b2a5d981f36b74bc19a9bbeb635b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-06-17 20:15:46 -0400

    program uses the struct created in struct.h to ask the user for the name and dorm of the total amount of students enrolled and writes the names to a csv file

---
+++ c/Student-Struct/struct.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "struct.h"
+
+int main(void)
+{
+ //allocating space for students
+
+ int enrollment = get_int("Enrollment: ");
+
+ student students[enrollment];
+
+ //prompt user to input students' name and dorm
+ for (int i = 0; i < enrollment; i++)
+ {
+    students[i].name = get_string("Name: ");
+    students[i].dorm = get_string("Dorm: ");
+ }
+
+ //save students to disk
+ FILE *file = fopen("students.csv", "w");
+
+ if (file)
+ {
+     for (int i = 0; i < enrollment; i++)
+     {
+        fprintf(file, "%s,%s\n", students[i].name, students[i].dorm);
+     }
+     fclose(file);
+
+ }
+}
