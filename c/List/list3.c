4eea27d7d1d62a367f2a8602a21396b0a2d8d7fd
Christian Gracia <christiangracia.student@careerdevs.com>
2019-06-20 18:37:02 -0400

    init of list method using nodes

---
+++ c/List/list3.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //memory for numbers
+
+    node *numbers = NULL;
+
+    //prompt for numbers (until EOF)
+    while(true)
+    {
+        //prompt for number
+        int number = get_int("number: ");
+
+        //check for EOF
+        if (number == INT_MAX)
+        {
+            break;
+        }
+
+        //allocate space for number
+
+        node *n = malloc(sizeof(node));
+
+        if (!n)
+        {
+            return 1;
+        }
+
+        //add number to list
+
+        n->number = number;
+        n->next = NULL;
+    }
+
+}
